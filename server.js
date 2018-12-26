d4eee36f3ee22d086b4d7e9908cff88c66a13203
Tiago Lopes Zenicola <tiagozenicola@gmail.com>
2018-12-26 19:58:27 -0200

    Create endpoint to fetch tables

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const getTables = require('./util')
+const fetch = require('node-fetch');
+
+
+app.get('/', function (req, res) {
+  fetch('https://www.theguardian.com/football/tables')
+    .then(response => {
+        if (!response.ok){
+            const error_message = 'Error calling site'
+            console.error(error_message);
+            throw new Error(error_message)
+        }
+
+        return response.text();
+    })
+    .then(data => {
+        const tables = getTables(data)
+        res.send(tables);
+    })
+    .catch(console.error);
+  
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
---
+++ util.js
@@ -0,0 +1,58 @@
+const HTMLParser = require('node-html-parser');
+
+const countries = ['england', 'spain', 'italy', 'germany','france']
+
+const removeWomenTable = (_, index) => {
+  return index !== 1
+}
+
+const getTables = data => {
+  const html = getAsHtml(data)
+  const tables = html.querySelectorAll('table')
+  const majorTables = Array.from(tables).slice(0, countries.length + 1).filter(removeWomenTable)
+
+  const mapTables = {}
+
+  majorTables.forEach((table, index) => {
+    mapTables[countries[index]] = getTeamsFromTable(table);
+  })
+
+  return mapTables;
+}
+
+const getTeamsFromTable = table => {
+
+  const teams = []
+
+  table.querySelectorAll('tr').forEach((row, index) => {
+
+    if (index === 0 || index === 5){
+      return
+    }
+
+    const columns = row.querySelectorAll('td')
+
+    teams.push({
+      "position": parseInt(columns[0].innerText),
+      "name": columns[1].innerText,
+      "games_played": parseInt(columns[2].innerText),
+      "win": parseInt(columns[3].innerText),
+      "drawn": parseInt(columns[4].innerText),
+      "lost": parseInt(columns[5].innerText),
+      "goals_for": parseInt(columns[6].innerText),
+      "goals_against": parseInt(columns[7].innerText),
+      "goal_difference": parseInt(columns[8].innerText),
+      "points": parseInt(columns[9].innerText),
+      "history": [],
+      "percent": columns[9].innerText / (columns[2].innerText * 3),
+    })
+
+  })
+  return teams
+}
+
+const getAsHtml = text => {
+  return HTMLParser.parse(text)
+}
+
+module.exports = getTables;
